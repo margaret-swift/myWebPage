eabc394f95d810b293cb3ebcafcfa7eaf938d9a9
mes114 <mes114@duke.edu>
2020-03-16 11:50:19 -0400

    latest webupdate

--- buildMe.sh
+++ buildMe.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo building web page...
 cd hostinger
 git pull
 cd ..
 ng build
+echo pushing web page files to hostinger git...
 cp dist/myWebPage hostinger
 cd hostinger
 git add .
 git commit -m 'latest webupdate'
 git push
+echo pushing angular files to personal git...
 cd ..
 git add .
 git commit -m 'latest webupdate'
