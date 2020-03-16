2266b537b9592e690bef0380475eb065ffda9dde
mes114 <mes114@duke.edu>
2020-03-16 11:51:36 -0400

    latest webupdate

--- buildMe.sh
+++ buildMe.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+# Automatically pull and push to GIT, as well as ng build
+echo ------------------------
 echo building web page...
-cd hostinger
-git pull
-cd ..
 ng build
+
 echo pushing web page files to hostinger git...
 cp dist/myWebPage hostinger
 cd hostinger
 git add .
 git commit -m 'latest webupdate'
 git push
+
 echo pushing angular files to personal git...
 cd ..
 git add .
 git commit -m 'latest webupdate'
 git push
+
+echo done
+echo ------------------------
+
+#eof
